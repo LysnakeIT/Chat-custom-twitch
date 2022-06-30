c8a8d5fcd2c57e1a3f0768478f0f0197af0f404c
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2022-06-30 15:15:47 +0200

    ✅Modification positionnement et taille

--- src/App.vue
+++ src/App.vue
@@ -80,20 +80,33 @@ export default {
 
 #app {
   width: 100%;
-  height: calc(100vh - 3vh);
+  height: calc(100vh - 7vh);
   padding: 0;
   margin: 0;
+  background: transparent;
+  font-family: "Montserrat", sans-serif;
+  color: white;
+  line-height: 1;
+}
+
+html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
 }
 
 .theme-lysnake {
   font-family: "Montserrat", sans-serif;
   margin-bottom: 32px;
-  width: 100%;
   height: 100%;
   max-height: 100vh;
   flex-direction: column;
   align-items: flex-end;
   justify-content: flex-end;
   display: flex;
+  padding: 32px;
 }
 </style>
